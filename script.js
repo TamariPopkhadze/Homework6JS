a88cfe0ba918ad71ad5ee21481588d409a74093d
TamariPopkhadze <popkhadzetamari@gmail.com>
2023-05-31 02:06:39 +0400

    part2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//--------------First Task-------------
+
 let header = document.querySelector('.header-style');
 let menu = document.querySelector('.menu');
 let ul = document.querySelector('.ulstyle');
-let div = document.querySelector('.div')
+
 menu.addEventListener('click',function(){
     header.classList.toggle('changeHeaderBackground')
     ul.classList.toggle('changemenu')
@@ -12,7 +14,7 @@ menu.addEventListener('click',function(){
     cross.classList.add('fa-2xl');
     cross.style.color = 'deeppink';
     cross.classList.add('cross');
-    div.appendChild(cross);
+    header.appendChild(cross);
     cross.addEventListener('click',function(){
         header.classList.toggle('changeHeaderBackground')
         ul.classList.toggle('changemenu')
@@ -21,3 +23,34 @@ menu.addEventListener('click',function(){
     })
 
 })
+
+//--------------Second Task-------------
+ let input = document.getElementById('input');
+ let button = document.getElementById('button');
+ let ullist = document.querySelector('.ullist');
+ let clearAll = document.querySelector('.clearallbutton');
+ let form = document.querySelector('.form');
+
+ form.addEventListener('submit', function(x){
+    x.preventDefault();
+    let value = input.value;
+    let list = document.createElement('li');
+    list.classList.add('listyle');
+    list.innerText = value;
+    let trash = document.createElement('i');
+    trash.classList.add('fa-regular');
+    trash.classList.add('fa-trash-can');
+    trash.classList.add('fa-xs');
+    trash.style.color ='deeppink';
+    ullist.appendChild(list);
+    trash.addEventListener('click', function(){
+        list.remove();
+    })
+    list.appendChild(trash)
+    input.value = " ";
+
+ })
+ clearAll.addEventListener('click', function(){
+    ullist.innerHTML = " ";
+ })
+
